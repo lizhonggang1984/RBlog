1d42c0819434db51564d53a1163a54bbea5c8700
Zhonggang Li <lizhonggang80@gmail.com>
2020-05-09 22:32:51 -0500

    changed

---
+++ public/config/deploy/production.rb
@@ -0,0 +1,5 @@
+set :domain, 'liuzhen.me'
+set :deploy_to, '/data/www/RBlog'
+set :repository,  'https://github.com/liuzhenangel/RBlog.git'
+set :branch, 'master'
+set :user, 'ruby'
---
+++ public/config/routes.rb
@@ -0,0 +1,35 @@
+require 'sidekiq/web'
+Sidekiq::Web.set :session_secret, Rails.application.secrets[:secret_key_base]
+
+Rails.application.routes.draw do
+
+  # write your routes here
+
+  mount Sidekiq::Web => '/sidekiq'
+  mount StatusPage::Engine => '/'
+  #mount ActionCable.server => '/cable'
+  root to: 'home#index'
+
+  get 'about', to: 'home#about'
+  get 'timeline', to: 'home#timeline'
+  get 'signout', to: 'sessions#destroy'
+
+  resources :sessions, only:[:new, :create]
+  resources :articles, only: [:index, :show]
+  resources :photos, only: [:index, :show]
+  namespace :admin do
+    root 'dashboard#index', as: 'root'
+		post '/upload', to: 'photos#upload'
+    resources :articles do
+      member do
+        get :push
+      end
+    end
+    resources :photos do
+      member do
+        get :push
+      end
+    end
+    resource :resume, only: [:edit, :update]
+  end
+end
